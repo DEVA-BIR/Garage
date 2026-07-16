7e43054fc17484bb376560e43398431599fa99d8
Dawit Birhanu <dawitbirhanu2323@gmail.com>
2026-07-16 18:07:35 +0300

    Set default port for database connection

--- BackEnd/Config/dbconfig.js
+++ BackEnd/Config/dbconfig.js
@@ -8,7 +8,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 10563 ,
 
  ssl: {
     rejectUnauthorized: false,
